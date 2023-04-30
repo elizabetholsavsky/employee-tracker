9210aea2cb5e562883964857e576519a5a7c7413
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-30 11:41:55 -0500

    update db schema and seeds, add foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,22 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30)
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,19 +7,23 @@ VALUES
 ('Management'), 
 ('Sales');
 
-INSERT INTO role (title, salary)
+INSERT INTO role (title, salary, department_id)
 VALUES 
-('Engineer', 100000), 
-('Accountant', 90000), 
-('Lawyer', 90000), 
-('IT Specialist', 70000), 
-('Project Manager', 120000), 
-('Sales', 60000);
+('Engineer', 100000, 1), 
+('Accountant', 90000, 2), 
+('Lawyer', 90000, 3), 
+('IT Specialist', 70000, 4), 
+('Project Manager', 120000, 5), 
+('Sales', 60000, 6);
 
-INSERT INTO employee (first_name, last_name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Syd', 'Barrett'),
-('Nick', 'Mason'),
-('Roger', 'Waters'),
-('Richard', 'Wright'),
-('David', 'Gilmour');
+('Syd', 'Barrett', 1, NULL),
+('Nick', 'Mason', 2, NULL),
+('Roger', 'Waters', 3, NULL),
+('Richard', 'Wright', 4, NULL),
+('David', 'Gilmour', 5, 5),
+('Robert', 'Plant', 6, NULL),
+('Jimmy', 'Page', 1, NULL),
+('John', 'Bonham', 2, NULL),
+('John Paul', 'Jones', 5, 9);
