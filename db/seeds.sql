e3e4566638e2ff1bc8eceec86e56d06d5cb7af7e
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-30 21:09:24 -0500

    change sales role to telemarketer in seeds (to differentiate from department and avoid confusion)

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,7 +14,7 @@ VALUES
 ('Lawyer', 90000, 3), 
 ('IT Specialist', 70000, 4), 
 ('Project Manager', 120000, 5), 
-('Sales', 60000, 6);
+('Telemarketer', 60000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
